adf5a780fa7d82b4470e1ac8361e7d828a4f2003
ProgramDyl <dylanitwork@gmail.com>
2025-02-07 18:37:20 -0400

    final

--- MusicForum/Controllers/HomeController.cs
+++ MusicForum/Controllers/HomeController.cs
@@ -17,19 +17,22 @@ namespace MusicForum.Controllers
 
         // home - show all discussion threads
         public async Task<IActionResult> Index()
-
-
         {
-            var discussions = await _context.Discussion.OrderByDescending(d => d.CreateDate).ToListAsync();
+            var discussions = await _context.Discussion
+                .Include(d => d.Comments) 
+                .OrderByDescending(d => d.CreateDate)
+                .ToListAsync();
 
-            return View(discussions); // pass in the discussion list to the view
+            return View(discussions); 
         }
+
 
         // discussion details - displays the details of a discussion
         public async Task<IActionResult> DiscussionDetails(int id)
         {
-            var discussion = await _context.Discussion.Include(m => m.Comments)
-                                                      .FirstOrDefaultAsync(m => m.DiscussionId == id);
+            var discussion = await _context.Discussion
+                .Include(m => m.Comments)
+                .FirstOrDefaultAsync(m => m.DiscussionId == id);
             return View(discussion);
         }
 
